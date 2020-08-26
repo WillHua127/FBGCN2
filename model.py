4a5e388c854705128d1d655c5b6d2c1f1b1b04a4
WillHua127 <Williamhuachenqing@Hotmail.com>
2020-08-26 18:00:36 -0400

    FBGCN2

---
+++ model.py
@@ -0,0 +1,125 @@
+import torch.nn as nn
+import torch
+import math
+import numpy as np
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+
+class GraphConvolution(nn.Module):
+
+    def __init__(self, in_features, out_features, residual=False, variant=False):
+        super(GraphConvolution, self).__init__() 
+        self.variant = variant
+        if self.variant:
+            self.in_features = 2*in_features 
+        else:
+            self.in_features = in_features
+
+        self.out_features = out_features
+        self.residual = residual
+        self.weight = Parameter(torch.FloatTensor(self.in_features,self.out_features))
+        self.weight_high = Parameter(torch.FloatTensor(self.in_features,self.out_features))
+        self.c = nn.Parameter(torch.zeros(size=(1, 1)))
+        torch.nn.init.uniform_(self.c.data, a=0, b=1)
+        self.c_high = nn.Parameter(torch.zeros(size=(1, 1)))
+        torch.nn.init.uniform_(self.c_high.data, a=0, b=1)
+        
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.out_features)
+        self.weight.data.uniform_(-stdv, stdv)
+        self.weight_high.data.uniform_(-stdv, stdv)
+        
+
+    def forward(self, input, adj, adj_high , h0 , lamda, alpha, l):
+        theta = math.log(lamda/l+1)
+        hi = torch.spmm(adj, input)
+        hi_high = torch.spmm(adj_high, input)
+        if self.variant:
+            support = torch.cat([hi,h0],1)
+            r = (1-alpha)*hi+alpha*h0
+            
+            support_high = torch.cat([hi_high,h0],1)
+            r_high = (1-alpha)*hi_high+alpha*h0
+        else:
+            support = (1-alpha)*hi+alpha*h0
+            r = support
+            
+            support_high = (1-alpha)*hi_high+alpha*h0
+            r_high = support
+            
+        output = theta*torch.mm(support, self.weight)+(1-theta)*r
+        output_high = theta*torch.mm(support_high, self.weight_high)+(1-theta)*r_high
+        output = (self.c*output + self.c_high*output_high) / (self.c+self.c_high)
+        if self.residual:
+            output = output+input
+        return output
+
+class GCNII(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha, variant):
+        super(GCNII, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.params1 = list(self.convs.parameters())
+        self.params2 = list(self.fcs.parameters())
+        self.act_fn = nn.ReLU()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+        
+    def c_value(self):
+        return self.convs[-1].c, self.convs[-1].c_high
+
+    def forward(self, x, adj, adj_high):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,adj_high,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.fcs[-1](layer_inner)
+        return F.log_softmax(layer_inner, dim=1)
+
+class GCNIIppi(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha,variant):
+        super(GCNIIppi, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant,residual=True))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.act_fn = nn.ReLU()
+        self.sig = nn.Sigmoid()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+
+    def forward(self, x, adj):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.sig(self.fcs[-1](layer_inner))
+        return layer_inner
+
+
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
